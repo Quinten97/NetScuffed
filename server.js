5af41436d91dd668c8fa5dd729d35d1a44817242
Quinten Favorite <quintenfavorite97@gmail.com>
2025-04-25 23:44:30 -0400

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route: Homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+// Route: About page
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+// Route: Contact page
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "contact.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
