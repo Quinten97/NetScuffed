794e21d96e9368792c8c6a6906b4ee91f548277d
Quinten Favorite <quintenfavorite97@gmail.com>
2025-04-28 16:27:48 -0400

    finished page routing and basic html templates.

--- server.js
+++ server.js
@@ -7,19 +7,32 @@ const PORT = 3000;
 // Serve static files from /public
 app.use(express.static(path.join(__dirname, "public")));
 
-// Route: Homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-// Route: About page
 app.get("/wifi", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "wifi.html"));
 });
 
-// Route: Contact page
 app.get("/lldp", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "lldp.html"));
+});
+
+app.get("/ping", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "ping.html"));
+});
+
+app.get("/speedtest", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "speedtest.html"));
+});
+
+app.get("/nmap", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "nmap.html"));
+});
+
+app.get("/wireshark", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "wireshark.html"));
 });
 
 app.listen(PORT, () => {
